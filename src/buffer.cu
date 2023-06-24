11740b82a54b22f0b68a433445d3e8f3aa6c0d06
Vicente González <vigb902@gmail.com>
2023-06-23 23:08:50 -0400

    cuda fix

---
+++ src/buffer.cu
@@ -0,0 +1,172 @@
+//
+// Created by Vicente González on 22-06-23.
+//
+#include <glad/glad.h>
+
+#include <random>
+
+#include <cuda_runtime.h>
+#include <cuda_gl_interop.h>
+
+#include "buffer.h"
+
+
+static std::random_device rd;
+static std::mt19937 gen(rd());
+static std::uniform_real_distribution<float> dis(-1.0, 1.0);
+
+static const float MASS = 1.0f;
+
+using namespace glm;
+
+Buffer::Buffer()
+	: _ebo(0), _vbo(0), _vao(0), data(), _ratio(0.0f), _s_mass(MASS), _size(0), _cuda_resource(nullptr)
+{
+	glGenVertexArrays(1, &_vao);
+	glGenBuffers(1, &_vbo);
+	glGenBuffers(1, &_ebo);
+}
+
+Buffer::~Buffer()
+{
+	glDeleteVertexArrays(1, &_vao);
+	glDeleteBuffers(1, &_vbo);
+	glDeleteBuffers(1, &_ebo);
+}
+
+void Buffer::bind() const
+{
+	glBindVertexArray(_vao);
+	glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _ebo);
+}
+
+void Buffer::unbind() const
+{
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+}
+
+
+void Buffer::build()
+{
+	unsigned int old_size = static_cast<unsigned int>(data.size());
+
+	std::vector<Point> newdata(_size);
+
+	auto n_sp = static_cast<unsigned int>(_size * _ratio);
+	unsigned int j = 0;
+
+	for (unsigned int i = 0; i < _size; ++i)
+	{
+		vec3 position(dis(gen), dis(gen), 0.0f);
+		vec3 velocity = {0.0f, 0.0f, 0.0f};
+		float mass = MASS;
+
+
+		if (i < old_size)
+		{
+			position = data[i].position;
+			velocity = data[i].velocity;
+		}
+
+
+		if (j < n_sp)
+		{
+			mass = _s_mass;
+			++j;
+		}
+
+		newdata[i] = {position, velocity, mass};
+	}
+
+	data = newdata;
+
+//	std::vector<float> vertices;
+//	std::vector<unsigned int> indices;
+//	vertices.reserve(_size * 6);
+//	for (const auto& point : data)
+//	{
+//		vertices.push_back(point.position.x);
+//		vertices.push_back(point.position.y);
+//		vertices.push_back(point.position.z);
+//		vertices.push_back(point.acceleration.x);
+//		vertices.push_back(point.acceleration.y);
+//		vertices.push_back(point.acceleration.z);
+//		indices.push_back(static_cast<unsigned int>(indices.size()));
+//	}
+
+	bind();
+	glBufferData(GL_ARRAY_BUFFER, _size * sizeof(Point), data.data(), GL_DYNAMIC_DRAW);
+	std::vector<unsigned int> indices(_size);
+	for (unsigned int i = 0; i < _size; ++i)
+		indices[i] = i;
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, _size * sizeof(unsigned int), indices.data(), GL_DYNAMIC_DRAW);
+
+	cudaGraphicsGLRegisterBuffer(&_cuda_resource, _vbo, cudaGraphicsMapFlagsNone);
+//	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
+//	glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int), indices.data(), GL_STATIC_DRAW);
+	unbind();
+}
+
+void Buffer::setLayout(const Shader& shader) const
+{
+	shader.bind();
+	bind();
+
+	auto pos = shader.getAttribLocation("position");
+	glEnableVertexAttribArray(pos);
+	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, sizeof(Point), nullptr);
+
+//	auto vel = shader.getAttribLocation("velocity");
+//	glEnableVertexAttribArray(vel);
+//	glVertexAttribPointer(vel, 3, GL_FLOAT, GL_FALSE, sizeof(Point), (const void*)(3 * sizeof(float)));
+//
+//	auto acc = shader.getAttribLocation("acceleration");
+//	glEnableVertexAttribArray(acc);
+//	glVertexAttribPointer(acc, 3, GL_FLOAT, GL_FALSE, sizeof(Point), (const void*)(6 * sizeof(float)));
+
+	auto mass = shader.getAttribLocation("mass");
+	glEnableVertexAttribArray(mass);
+	glVertexAttribPointer(mass, 1, GL_FLOAT, GL_FALSE, sizeof(Point), (const void*)(6 * sizeof(float)));
+
+	unbind();
+	shader.unbind();
+}
+
+
+float* Buffer::getCudaPoints() const 
+{
+	size_t num_bytes = _size * sizeof(Point);
+	cudaGraphicsResourceGetMappedPointer((void**)&_cuda_points, &num_bytes, _cuda_resource);
+	return _cuda_points;
+}
+
+void Buffer::map()
+{
+	cudaGraphicsMapResources(1, &_cuda_resource, 0);
+}
+
+void Buffer::unmap()
+{
+	// copy data from cuda to host
+	cudaMemcpy(data.data(), _cuda_points, _size * sizeof(Point), cudaMemcpyDeviceToHost);
+	cudaGraphicsUnmapResources(1, &_cuda_resource, 0);
+}
+
+std::ostream& operator<<(std::ostream& os, const Buffer& buffer)
+{
+	os << "Buffer: " << std::endl;
+	os << "Size: " << buffer._size << std::endl;
+	os << "Ratio: " << buffer._ratio << std::endl;
+	os << "Data: " << std::endl;
+	for (const auto& point : buffer.data)
+	{
+		os << point.position.x << ", " << point.position.y << ", " << point.position.z << std::endl;
+		os << std::endl;
+	}
+	return os;
+}
+
---
+++ src/main.cu
@@ -0,0 +1,299 @@
+// Based on Imgui example for GLFW + OpenGL3
+// Read online: https://github.com/ocornut/imgui/tree/master/docs
+
+#define GL_SILENCE_DEPRECATION
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h> // Will drag system OpenGL headers
+
+#include <glm/vec3.hpp>
+#include <glm/geometric.hpp>
+
+#include <iostream>
+#include <vector>
+
+#include "shader.h"
+#include "buffer.h"
+
+
+#define GRAVITY 6.67408e-6f
+#define SOFTENING 1e-3f
+
+#define BLOCK_SIZE 512
+
+static void glfw_error_callback(int error, const char* description)
+{
+	std::cerr << "GLFW Error " << error << ": " << description << std::endl;
+}
+
+static void simulate(std::vector<Point>& points, float dt)
+{
+	using namespace glm;
+
+	for (auto& p1 : points)
+	{
+		auto acceleration = vec3(0.0f);
+		for (auto& p2 : points)
+		{
+			if (&p1 == &p2)
+				continue;
+
+			auto r = p2.position - p1.position;
+			auto distSqr = dot(r, r);
+			auto force = GRAVITY * p2.mass / (distSqr + SOFTENING);
+
+
+			acceleration += force * normalize(r);
+		}
+
+		p1.velocity += acceleration * dt;
+	}
+
+
+}
+
+static void update(std::vector<Point>& points, float dt)
+{
+	for (auto& p : points)
+	{
+		p.position += p.velocity * dt;
+	}
+}
+
+__global__ void simulate_cuda(float* points, float dt, int n)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx >= n)
+		return;
+	float p_x = points[idx * 7];
+	float p_y = points[idx * 7 + 1];
+	float p_z = points[idx * 7 + 2];
+
+
+	float a_x = 0.0f;
+	float a_y = 0.0f;
+	float a_z = 0.0f;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (i == idx)
+			continue;
+
+
+		float r[3] = { points[i * 7] - p_x, points[i * 7 + 1] - p_y, points[i * 7 + 2] - p_z };
+		float distSqr = r[0] * r[0] + r[1] * r[1] + r[2] * r[2];
+		float dist = sqrtf(distSqr);
+		float force = GRAVITY * points[7*i + 6] / (distSqr + SOFTENING);
+
+		float r_unit[3] = { r[0] / dist, r[1] / dist, r[2] / dist };
+
+		a_x += force * r_unit[0];
+		a_y += force * r_unit[1];
+		a_z += force * r_unit[2];
+	}
+
+	points[idx*7 + 3] += a_x * dt;
+	points[idx*7 + 4] += a_y * dt;
+	points[idx*7 + 5] += a_z * dt;
+}
+
+__global__ void update_cuda(float* points, float dt, int n)
+{
+	int idx = blockIdx.x * blockDim.x + threadIdx.x;
+	if (idx >= n)
+		return;
+	points[idx * 7] += points[idx * 7 + 3] * dt;
+	points[idx * 7 + 1] += points[idx * 7 + 4] * dt;
+	points[idx * 7 + 2] += points[idx * 7 + 5] * dt;
+}
+
+// Main code
+int main(int, char**)
+{
+	glfwSetErrorCallback(glfw_error_callback);
+	if (!glfwInit())
+		return 1;
+
+	// Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+	// GL ES 2.0 + GLSL 100
+	const char* glsl_version = "#version 100";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+	// GL 3.2 + GLSL 150
+	const char* glsl_version = "#version 330";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+	// GL 3.0 + GLSL 130
+	const char* glsl_version = "#version 130";
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+	//glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	// Create window with graphics context
+	GLFWwindow* window = glfwCreateWindow(800, 800, "Dear ImGui GLFW+OpenGL3 example", nullptr, nullptr);
+	if (window == nullptr)
+		return 1;
+	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable vsync
+
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
+	// Setup Platform/Renderer backends
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init(glsl_version);
+
+	// Our state
+	bool use_quads = false;
+	bool pause = true;
+	bool use_gpu = false;
+
+	float mass = 30.0f;
+	float ratio = 0.1f;
+	int n = 100;
+	ImVec4 clear_color = ImVec4(0.0f, 0.0f, 0.0f, 1.00f);
+
+	// Create a shader program
+	Shader shader("shaders/nbody.vert", "shaders/nbody.geom", "shaders/nbody.frag");
+	Buffer buffer;
+	buffer.setLayout(shader);
+
+	auto now = static_cast<float>(glfwGetTime());
+	auto last = now;
+	float delta;
+	while (!glfwWindowShouldClose(window))
+	{
+		// simulation
+		now = static_cast<float>(glfwGetTime());
+		delta = now - last;
+		last = now;
+
+
+		if (!pause)
+		{
+			if (use_gpu)
+			{
+				buffer.map();
+				simulate_cuda << < (n + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE >> > (buffer.getCudaPoints(), delta, n);
+				update_cuda << < (n + BLOCK_SIZE - 1) / BLOCK_SIZE, BLOCK_SIZE >> > (buffer.getCudaPoints(), delta, n);
+				buffer.unmap();
+			}
+			else
+			{
+				simulate(buffer.data, delta);
+				update(buffer.data, delta);
+			}
+
+		}
+
+
+
+		glfwPollEvents();
+		// Start the Dear ImGui frame
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+
+		// 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
+		{
+			ImGui::Begin("Simulation options");                          // Create a window called "Hello, world!" and append into it.
+
+//			ImGui::Checkbox("Pause", &pause);      // Edit bools storing our window open/close state
+
+
+			ImGui::Checkbox("View quads?", &use_quads);      // Edit bools storing our window open/close state
+			ImGui::SameLine();
+			ImGui::Checkbox("Use GPU?", &use_gpu);      // Edit bools storing our window open/close state
+
+			ImGui::SliderInt("Number of points", &n, 0, 5000);
+			// Vec2 input
+			ImGui::SliderFloat("Special mass", &mass, 1.0f, 100.0f, "%.3f");
+
+			// ImGui::ColorEdit("Special Gravity", &acceleration, 0.1f, 1.0f, "%.3f");
+			ImGui::SliderFloat("SM Ratio",
+				&ratio,
+				0.01f,
+				1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+			ImGui::ColorEdit3("Clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+			const std::string& label = pause ? "Play" : "Pause";
+			if (ImGui::Button(label.c_str()))
+				pause = !pause;
+
+			ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+			ImGui::End();
+		}
+
+
+		buffer.setSize(n);
+		buffer.setSpecialMass(mass);
+		buffer.setRatio(ratio);
+		buffer.build();
+
+		// Rendering
+		ImGui::Render();
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(clear_color.x * clear_color.w,
+			clear_color.y * clear_color.w,
+			clear_color.z * clear_color.w,
+			clear_color.w);
+
+		shader.bind();
+
+		shader.setUniform1i("useQuads", use_quads);
+
+		buffer.bind();
+
+		glDrawElements(GL_POINTS, n, GL_UNSIGNED_INT, 0);
+
+		buffer.unbind();
+		shader.unbind();
+
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(window);
+	}
+
+	// Cleanup
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	return 0;
+}
